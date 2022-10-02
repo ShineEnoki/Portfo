f1560d7ad884abc0a88be2812470bd36ffe8b713
Jiraiya <thuriyac4@gmail.com>
2022-10-02 18:49:36 +0630

    make the death note project photo change

--- DeathNoteProject/script.js
+++ DeathNoteProject/script.js
@@ -11,12 +11,14 @@ function getRandomInt() {
     } else {
       randomNum = generateNumber;
     }
+
+    let x = document.querySelector("#playerNumber").textContent;
+
     document
         .querySelector("#reply")
-        .textContent = "Your number is " + randomNum;
+        .textContent = x + " got '" + randomNum + "'.";
     
     // Whose turn now
-    let x = document.querySelector("#playerNumber").textContent;
     if ( x === "Player1") {
       document.querySelector("#playerNumber").textContent = "Player2";
     } else {
@@ -25,105 +27,52 @@ function getRandomInt() {
 
     // change image
 
-    // for player2
-    if (x === "Player1") {
-      if (randomNum === 1) {
-        let teamtest = document.querySelector("#blue1").src.substr(-5,1) ;
-        if (teamtest > 8) {
-          document.querySelector("#blue1").src = "photo/6.png";
-        } else {
-          document.querySelector("#blue1").src = `photo/${parseInt(teamtest)+1}.png`;
-        }
-      }
-
-      if (randomNum === 3) {
-        let teamtest = document.querySelector("#blue2").src.substr(-5,1) ;
-        if (teamtest > 8) {
-          document.querySelector("#blue2").src = "photo/6.png";
-        } else {
-          document.querySelector("#blue2").src = `photo/${parseInt(teamtest)+1}.png`;
-        }
-      }
-
-      if (randomNum === 5) {
-        let teamtest = document.querySelector("#blue3").src.substr(-5,1) ;
-        if (teamtest > 8) {
-          document.querySelector("#blue3").src = "photo/6.png";
-        } else {
-          document.querySelector("#blue3").src = `photo/${parseInt(teamtest)+1}.png`;
-        }
-      }
-
-      if (randomNum === 7) {
-        let teamtest = document.querySelector("#blue4").src.substr(-5,1) ;
-        if (teamtest > 8) {
-          document.querySelector("#blue4").src = "photo/6.png";
-        } else {
-          document.querySelector("#blue4").src = `photo/${parseInt(teamtest)+1}.png`;
-        }
-      }
-
-      if (randomNum === 9) {
-        let teamtest = document.querySelector("#blue5").src.substr(-5,1) ;
-        if (teamtest > 8) {
-          document.querySelector("#blue5").src = "photo/6.png";
-        } else {
-          document.querySelector("#blue5").src = `photo/${parseInt(teamtest)+1}.png`;
-        }
-      }
-    }
-
-    // for player 1
-    if (x === "Player2") {
-      if (randomNum === 1) {
-        let teamtest = document.querySelector("#red1").src.substr(-5,1) ;
-        if (teamtest > 8) {
-          document.querySelector("#red1").src = "photo/6.png";
-        } else {
-          document.querySelector("#red1").src = `photo/${parseInt(teamtest)+1}.png`;
-        }
-      }
-
-      if (randomNum === 3) {
-        let teamtest = document.querySelector("#red2").src.substr(-5,1) ;
-        if (teamtest > 8) {
-          document.querySelector("#red2").src = "photo/6.png";
-        } else {
-          document.querySelector("#red2").src = `photo/${parseInt(teamtest)+1}.png`;
-        }
-      }
-
-      if (randomNum === 5) {
-        let teamtest = document.querySelector("#red3").src.substr(-5,1) ;
-        if (teamtest > 8) {
-          document.querySelector("#red3").src = "photo/6.png";
-        } else {
-          document.querySelector("#red3").src = `photo/${parseInt(teamtest)+1}.png`;
-        }
-      }
-
-      if (randomNum === 7) {
-        let teamtest = document.querySelector("#red4").src.substr(-5,1) ;
-        if (teamtest > 8) {
-          document.querySelector("#red4").src = "photo/6.png";
-        } else {
-          document.querySelector("#red4").src = `photo/${parseInt(teamtest)+1}.png`;
-        }
-      }
-
-      if (randomNum === 9) {
-        let teamtest = document.querySelector("#red5").src.substr(-5,1) ;
-        if (teamtest > 8) {
-          document.querySelector("#red5").src = "photo/6.png";
-        } else {
-          document.querySelector("#red5").src = `photo/${parseInt(teamtest)+1}.png`;
+    // for player1
+    if ( x === "Player1") {
+      let heroArray = [1, 3, 5, 7, 9];
+      let heroIndex ;
+      for (heroIndex in heroArray) {
+        if (randomNum == heroArray[heroIndex]) {
+          let heroicon = document.querySelector(`#blue${randomNum}`).src.substr(-5, 1);
+          if(heroicon > 8) {
+            document.querySelector(`#blue${randomNum}`).src = "photo/6.png"; //ဒီကစ ပြင်
+            document.querySelector("#playerNumber").textContent = "Player1";
+            document.querySelector("button").addEventListener("click", killOppositeHero) 
+              function killOppositeHero() {
+                let generateNumber = Math.floor(Math.random() * 10);
+                let randomNum ;
+                if (generateNumber % 2 == 0) {
+                  randomNum = generateNumber + 1;
+                } else {
+                  randomNum = generateNumber;
+                }
+                document.querySelector(`#red${randomNum}`).src = "#";
+              }                                                               //ဒီထိ
+          } else {
+            document.querySelector(`#blue${randomNum}`).src = `photo/${parseInt(heroicon)+1}.png`;
+          }
         }
       }
     }
     
-    // if (randomNum === 1 && x === "Player1") {
-    //   if 
-    // }
+
+    // for player 2
+    if ( x === "Player2") {
+      let heroArray = [1, 3, 5, 7, 9];
+      let heroIndex ;
+      for (heroIndex in heroArray) {
+        if (randomNum == heroArray[heroIndex]) {
+          let heroicon = document.querySelector(`#red${randomNum}`).src.substr(-5, 1);
+          if(heroicon > 8) {
+            document.querySelector(`#red${randomNum}`).src = "photo/6.png";
+          } else {
+            document.querySelector(`#red${randomNum}`).src = `photo/${parseInt(heroicon)+1}.png`;
+          }
+        }
+      }
+    }
+    
+
 }
 
 // For any better alogrithm or idea
--- DeathNoteProject/test.js
+++ DeathNoteProject/test.js
@@ -7,3 +7,51 @@ if (randomNum % 2 === 0) {
 }
 
 console.log(randomNum)
+
+// for player 1
+// if (x === "Player1") {
+    //   if (randomNum === 1) {
+    //     let teamtest = document.querySelector("#blue1").src.substr(-5,1) ;
+    //     if (teamtest > 8) {
+    //       document.querySelector("#blue1").src = "photo/6.png";
+    //     } else {
+    //       document.querySelector("#blue1").src = `photo/${parseInt(teamtest)+1}.png`;
+    //     }
+    //   }
+
+    //   if (randomNum === 3) {
+    //     let teamtest = document.querySelector("#blue2").src.substr(-5,1) ;
+    //     if (teamtest > 8) {
+    //       document.querySelector("#blue2").src = "photo/6.png";
+    //     } else {
+    //       document.querySelector("#blue2").src = `photo/${parseInt(teamtest)+1}.png`;
+    //     }
+    //   }
+
+    //   if (randomNum === 5) {
+    //     let teamtest = document.querySelector("#blue3").src.substr(-5,1) ;
+    //     if (teamtest > 8) {
+    //       document.querySelector("#blue3").src = "photo/6.png";
+    //     } else {
+    //       document.querySelector("#blue3").src = `photo/${parseInt(teamtest)+1}.png`;
+    //     }
+    //   }
+
+    //   if (randomNum === 7) {
+    //     let teamtest = document.querySelector("#blue4").src.substr(-5,1) ;
+    //     if (teamtest > 8) {
+    //       document.querySelector("#blue4").src = "photo/6.png";
+    //     } else {
+    //       document.querySelector("#blue4").src = `photo/${parseInt(teamtest)+1}.png`;
+    //     }
+    //   }
+
+    //   if (randomNum === 9) {
+    //     let teamtest = document.querySelector("#blue5").src.substr(-5,1) ;
+    //     if (teamtest > 8) {
+    //       document.querySelector("#blue5").src = "photo/6.png";
+    //     } else {
+    //       document.querySelector("#blue5").src = `photo/${parseInt(teamtest)+1}.png`;
+    //     }
+    //   }
+    // }
