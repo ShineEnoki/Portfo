901d4f95fced41946027ca0cc26d60c840dc04c0
JiraiyaOgata <thuriyac4@gmail.com>
2022-10-24 18:08:57 +0630

    This is version 1

--- DeathNoteProject/script.js
+++ DeathNoteProject/script.js
@@ -31,55 +31,77 @@ function getRandomInt() {
     
 
     // change image
-
-    // for player1
     let x = document.querySelector("#playerNumber").textContent;
 
+    // for player1
     if ( x === "Player1") {
-        let heroArray = [1, 3, 5, 7, 9];
-        let heroIndex ;
+        let heroicon = document.querySelector(`#blue${randomNum}`).src.substr(-5, 1);
+        if(heroicon > 8) {
+            document.querySelector("#playerNumber").textContent = "Player1";
 
-        for (heroIndex in heroArray) {
-            if (randomNum == heroArray[heroIndex]) {
-                let heroicon = document.querySelector(`#blue${randomNum}`).src.substr(-5, 1);
-                if(heroicon > 8) {
-                    document.querySelector(`#blue${randomNum}`).src = "photo/6.png"; //ဒီကစ ပြင်
-                    document.querySelector("#playerNumber").textContent = "Player1";
-                    document.querySelector("button").addEventListener("click", killOppositeHero) ;
+            document.getElementById("fight").style.display = "none";
+            document.getElementById("kill").style.display = "flex";
 
-                    function killOppositeHero() {
+            document.querySelector("#kill").addEventListener("click", killOppositeHero) ;
 
-                        let generateNumber = Math.floor(Math.random() * 10);
-                        let randomNum ;
-                        if (generateNumber % 2 == 0) {
-                            randomNum = generateNumber + 1;
-                        } else {
-                            randomNum = generateNumber;
-                        }
-                    document.querySelector(`#red${randomNum}`).src = "photo/10.png";
-                    }                                                               //ဒီထိ
+            function killOppositeHero() {
+                let generateNumber = Math.floor(Math.random() * 10);
+                let randomNum ;
+                if (generateNumber % 2 == 0) {
+                    randomNum = generateNumber + 1;
                 } else {
-                    document.querySelector(`#blue${randomNum}`).src = `photo/${parseInt(heroicon)+1}.png`;
+                    randomNum = generateNumber;
                 }
+
+                document.querySelector(`#red${randomNum}`).src = "photo/10.png";
+                document.querySelector(`#blue${randomNum}`).src = "photo/6.png"; 
+
+                document.getElementById("fight").style.display = "flex";
+                document.getElementById("kill").style.display = "none";
+            }                                                               
+        } else {
+            document.querySelector(`#blue${randomNum}`).src = `photo/${parseInt(heroicon)+1}.png`;
         }
-      }
+        
+        
+      
     }
     
 
     // for player 2
+
+
     if ( x === "Player2") {
-      let heroArray = [1, 3, 5, 7, 9];
-      let heroIndex ;
-      for (heroIndex in heroArray) {
-        if (randomNum == heroArray[heroIndex]) {
-          let heroicon = document.querySelector(`#red${randomNum}`).src.substr(-5, 1);
-          if(heroicon > 8) {
-            document.querySelector(`#red${randomNum}`).src = "photo/6.png";
-          } else {
+        let heroicon = document.querySelector(`#red${randomNum}`).src.substr(-5, 1);
+        if(heroicon > 8) {
+            document.querySelector("#playerNumber").textContent = "Player2";
+
+            document.getElementById("fight").style.display = "none";
+            document.getElementById("kill").style.display = "flex";
+
+            document.querySelector("#kill").addEventListener("click", killOppositeHero) ;
+
+            function killOppositeHero() {
+                let generateNumber = Math.floor(Math.random() * 10);
+                let randomNum ;
+                if (generateNumber % 2 == 0) {
+                    randomNum = generateNumber + 1;
+                } else {
+                    randomNum = generateNumber;
+                }
+
+                document.querySelector(`#blue${randomNum}`).src = "photo/10.png";
+                document.querySelector(`#red${randomNum}`).src = "photo/6.png"; 
+
+                document.getElementById("fight").style.display = "flex";
+                document.getElementById("kill").style.display = "none";
+            }                                                               
+        } else {
             document.querySelector(`#red${randomNum}`).src = `photo/${parseInt(heroicon)+1}.png`;
-          }
         }
-      }
+        
+        
+      
     }
 
     GetNumberAndTurn(randomNum);
